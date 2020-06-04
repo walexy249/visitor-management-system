df3d65df806c86e239b361e47f3b99f9d0c0e458
lawal olawale <lawalolawale32@gmail.com>
2020-06-04 23:46:51 +0100

    coding of the createNewUser route

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,31 @@
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator/check');
+const User = require('./../model/userModel');
+
 exports.getLoginPage = (req, res, next) => {
   res.render('login', {
     pageTitle: 'Login'
   });
 };
 
-exports.createNewUser = (req, res, next) => {};
+exports.createNewUserPage = (req, res, next) => {
+  res.render('create-new-user', {
+    pageTitle: 'create user'
+  });
+};
+
+exports.createUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // console.log(errors.array()[0].msg);
+    return res.redirect('/admin/create-new-user');
+  }
+  const hashedPassword = await bcrypt.hash(req.body.password, 12);
+  console.log(hashedPassword);
+  await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  });
+  res.redirect('/admin/login');
+};
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,8 +1,43 @@
 const express = require('express');
+const { body } = require('express-validator/check');
+
+const User = require('./../model/userModel');
+
 const authcontroller = require('./../controller/authController');
 
 const router = express.Router();
 
 router.route('/login').get(authcontroller.getLoginPage);
+
+router
+  .route('/create-new-user')
+  .get(authcontroller.createNewUserPage)
+  .post(
+    [
+      body('name')
+        .isLength({ min: 5 })
+        .withMessage('fullname must be more than 5 chracters')
+        .trim(),
+      body('email')
+        .isEmail()
+        .custom(async (value, { req }) => {
+          const user = await User.findOne({ email: req.body.email });
+          if (user) {
+            return Promise.reject(new Error('Email already exist'));
+          }
+        }),
+      body('password')
+        .isLength({ min: 5 })
+        .withMessage('password must be more at least 5 characters long'),
+      body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('password must match');
+        } else {
+          return true;
+        }
+      })
+    ],
+    authcontroller.createUser
+  );
 
 module.exports = router;
