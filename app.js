dea412853cb4912e0b02201e5a01119a10f3fcb0
lawal olawale <lawalolawale32@gmail.com>
2020-06-04 14:34:03 +0100

    configuring ejs templates

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // app.use((req, res, next) => {
 //   res.send('<h1>welcome page</h1>');
 // });
+// set the bodyparser
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // configuring the template engine to Ejs
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(3000, () => {
   console.log('app started on port 3000');
 });
